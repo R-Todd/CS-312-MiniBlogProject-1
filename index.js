5c0ae6423e5f59e24c2bf8cb65dadae18d450da0
Ryan Todd <r2neatha678@gmail.com>
2025-09-13 16:19:34 -0700

    Initialize Express app with basic setup
    Added index.js with a simple Express server responding to '/' with 'Hello World!'. Created package.json with express and ejs dependencies for future development.

---
+++ index.js
@@ -0,0 +1,26 @@
+// Set the app to use "express"
+const express = require('express');
+// Create an instance of express
+const app = express();
+
+// Set the port constant
+const port = 3000;
+
+/* === app.get ===
+"/" - url path
+2 arguments
+    a) req - request object
+    b) res - response object
+res.send() - sends a response to the client
+*/
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Make the app listen on the specified port
+
+app.listen(port, () => {
+    // log console to start server
+    console.log(`Server is running at http://localhost:${port}`);
+});
