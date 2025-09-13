799c26515d4f0c364e56d7f9abb33189fd3151b2
Ryan Todd <r2neatha678@gmail.com>
2025-09-13 16:52:31 -0700

    Add post creation form and route
    Introduces a form in index.ejs for creating new blog posts and adds a POST /create-post route in index.js to handle form submissions, updating the posts array and redirecting to the homepage. Also enables URL-encoded body parsing middleware for form data.

--- index.js
+++ index.js
@@ -3,30 +3,56 @@ const express = require('express');
 // Create an instance of express
 const app = express();
 
+// Middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+
 // Link View Engine
 app.set('view engine', 'ejs');
 
 // Set the port constant
 const port = 3000;
 
-// Set post array
+// === Set post array ===
 const posts = [
     {
-        title: "Post 1",
-        content: "This is the content of post 1.",
+        title: "Post 1", content: "This is the content of post 1.",
     },
     {
-        title: "Post 2",
-        content: "This is the content of post 2.",
-    },
+        title: "Post 2", content: "This is the content of post 2.",
+    }
 ];
+// === END post array ===
+
+
+
+//======== Create post route handler ========
+    
+   
+// Create post route handler
+app.post('/create-post', (req, res) => {
+    // 1. Get title and conent from req.body
+    const newPostTitle = req.body.title;
+    const newPostContent = req.body.content;
+
+    // 2. Create a new post object
+    const newPost = {title: newPostTitle, content: newPostContent};
+
+    // 3. Add the new post to the array using ".push(postObject)"
+    posts.push(newPost);
+
+    // 4. Redirect to the home page
+    res.redirect('/');
+});
+
+//=== end of create post route handler ===
+
+
 /* === app.get ===
-Step 1) "/" - url path
-Step 2) 2 arguments
-    a) req - request object
-    b) res - response object
-Step 3) res.send() - sends a response to the client
-*/
+    Step 1) "/" - url path
+    Step 2) 2 arguments
+        a) req - request object
+        b) res - response object
+    Step 3) res.send() - sends a response to the client*/
 
 app.get('/', (req, res) => {
     // Render the index.ejs file
@@ -34,10 +60,15 @@ app.get('/', (req, res) => {
         // pass the posts array to the template ({ posts: posts })
     res.render('index', { posts: posts });
 });
+// === END app.get ===
 
+
+// === app.listen ===
 // Make the app listen on the specified port
 
 app.listen(port, () => {
     // log console to start server
     console.log(`Server is running at http://localhost:${port}`);
 });
+
+// === END app.listen ===
