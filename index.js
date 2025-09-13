88c219264e5022191f91b94f748bb8bfddb246f1
Ryan Todd <r2neatha678@gmail.com>
2025-09-13 16:37:53 -0700

    Render blog posts on homepage
    Replaces the static 'Hello World' message with dynamic rendering of a posts array on the index page. The posts are passed from the server to the EJS template and displayed as blog entries.

--- index.js
+++ index.js
@@ -9,16 +9,30 @@ app.set('view engine', 'ejs');
 // Set the port constant
 const port = 3000;
 
+// Set post array
+const posts = [
+    {
+        title: "Post 1",
+        content: "This is the content of post 1.",
+    },
+    {
+        title: "Post 2",
+        content: "This is the content of post 2.",
+    },
+];
 /* === app.get ===
-"/" - url path
-2 arguments
+Step 1) "/" - url path
+Step 2) 2 arguments
     a) req - request object
     b) res - response object
-res.send() - sends a response to the client
+Step 3) res.send() - sends a response to the client
 */
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    // Render the index.ejs file
+        // register "index.ejs" as the template
+        // pass the posts array to the template ({ posts: posts })
+    res.render('index', { posts: posts });
 });
 
 // Make the app listen on the specified port
